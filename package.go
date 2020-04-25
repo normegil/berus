2982c61b29853bb490bc6ff3d7d9ccf9bac0e8b7
Olivier Ory <mail@normegil.be>
2020-04-25 17:37:05 +0200

    Add package-level godoc

--- package.go
+++ package.go
@@ -1,1 +1,3 @@
+// berus is a package containing presets and helpers that I often use for github/spf13/viper.
+// It provide some helpers to interface with github.om/spf13/cobra as well.
 package berus
